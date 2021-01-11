3976dd22b59370c3b6e4a935e6bff1a02ecbbdd9
alessandroperrone5 <73820833+alessandroperrone5@users.noreply.github.com>
2021-01-11 10:44:59 +0100

    Create R_code_exam_simulation.r

---
+++ R_code_exam_simulation.r
@@ -0,0 +1,26 @@
+#R code for the exam
+# https://land.copernicus.vgt.vito.be/PDF/portal/Application.html
+
+#nc data: we need library
+#https://cran.r-project.org/web/packages/ncdf4/index.html
+
+library(ncdf4)
+library(raster)
+setwd("C:/lab/") 
+
+#importing images of temperature of soil in January
+tjan <- raster("c_gls_LST10-DC_202101010000_GLOBE_GEO_V1.2.1.nc")
+plot(tjan)
+#change colorramppalette 
+cltjan <- colorRampPalette(c('black','grey','light grey'))(100) #
+plot(tjan, col=cltjan)
+
+#importing images of temperature of soil in October
+toct <- raster("c_gls_LST10-DC_202010010000_GLOBE_GEO_V1.2.1.nc")
+plot(toct, col=cltjan)
+cltoct <- colorRampPalette(c('blue','yellow','red'))(100) #
+
+#difference in temperature 
+dift <- tjan - toct
+cldif <- colorRampPalette(c('blue','white','red'))(100) #
+plot(dift, col=cldif)
