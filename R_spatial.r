973f080e784d7c7843ce65a27b0b1e6edd5ddca9
alessandroperrone5 <73820833+alessandroperrone5@users.noreply.github.com>
2021-01-17 16:41:08 +0100

    Update R_spatial.r

--- R_spatial.r
+++ R_spatial.r
@@ -1,18 +1,27 @@
+#R spatial
+
 library(sp)
+#load dataset 
 data(meuse)
 head(meuse)
 
+#to group coordinates and explain to R that the coordinates are x and y
 coordinates(meuse) = ~x+y
+#to see the set in space
 plot(meuse)
 
 #spplot is used to plot elements like zinc, etc. spread in space
 spplot(meuse, "zinc")
 #to give a title
 spplot(meuse, "zinc", main="Concentration of zinc")
+
+#exercise: plot concentration of copper
+spplot(meuse, "copper", main="Concentration of copper")
+
 #see copper and zinc together
 spplot(meuse, c("copper", "zinc"))
 
-#rather than use colors, we use bubbles
+#rather than use colors, we use bubbles (with different sizes depending on value of elements)
 bubble(meuse, "zinc")
 
 #color of bubbles
