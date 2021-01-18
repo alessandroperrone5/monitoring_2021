5b8e7edda7ddb762aae379439fe5647f5b1f6d13
alessandroperrone5 <73820833+alessandroperrone5@users.noreply.github.com>
2021-01-18 17:51:52 +0100

    Update R_code_multivar.r

--- R_code_multivar.r
+++ R_code_multivar.r
@@ -1,3 +1,4 @@
+#multivariate analysis to reduce the dimensionality of the plots and explain most of variability, showing it in just 2 axis
 #R_code_multivar.r
 install.packages("vegan")
 setwd("C:/lab/")
@@ -7,19 +8,19 @@ ls()
 
 #plot per species matrix
 head(biomes)
--->reduce the 6axis showed with the function: head(biomes), in 2 axes-->see the space in just 2 dimensions--->we will see how these species are connected each other
+#-->reduce the 6axis showed with the function: head(biomes), in 2 axes-->see the space in just 2 dimensions--->we will see how these species are connected each other
 
-#decorana---> Detrended correspondence analysis
+#decorana---> Detrended correspondence analysis --> to se relationships among several elements
 multivar <- decorana(biomes) 
 multivar
 plot(multivar)
 head(biomes_types)
 --> #now plot are not axes, arrows, but points in the graphs -->  
-        chestnut beech giant_orb maple_acer rafflesia lichens brown_bear oak lianas  
-      1      1     0         0          0         0       0          0   0      0
+  #      chestnut beech giant_orb maple_acer rafflesia lichens brown_bear oak lianas  
+  #   1      1     0         0          0         0       0          0   0      0
 #now is --->        
-         A1   tcode      type
-      1   1     1      temperate
+ #        A1   tcode      type
+ #    1   1     1      temperate
 
 #biomes names in the graph:
 attach(biomes_types)
@@ -39,4 +40,4 @@ ordispider(multivar, type, col=c("black","red","green","blue"), label = T)
 dev.off()
 
 #So now what you are able to do is if you have data with the X&Y, you can do point pattern analysis to monitor the change in time and space of your data.
-if you have plot per species, you can do multivariate analysis like this to see how is it going to change in time and even in space
+#if you have plot per species, you can do multivariate analysis like this to see how is it going to change in time and even in space
